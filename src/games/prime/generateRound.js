0b43678a9b084f685997b06f93ddb3143b146d27
Alexandra <varyandis@yandex.ru>
2025-07-15 20:09:49 +0300

    fix progression

--- src/games/prime/generateRound.js
+++ src/games/prime/generateRound.js
@@ -1,28 +1,23 @@
 import { maxRandomNumber, NO, YES } from '../../const.js'
 
 const isPrime = (num) => {
-  if (num < 2 || num % 2 === 0) {
-    return false
+  if (num < 2) return false
+  if (num === 2) return true
+  if (num % 2 === 0) return false
+
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false
   }
-  else if (num === 2) {
-    return true
-  }
-  else if (num > 3) {
-    for (let i = 3; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) {
-        return false
-      }
-    }
-  }
+
   return true
 }
 
 export const generateRound = () => {
   const question = Math.floor(Math.random() * maxRandomNumber + 1)
-  let correctAnswer = isPrime(question) ? YES : NO
+  const correctAnswer = isPrime(question) ? YES : NO
 
   return {
-    question: question,
-    correctAnswer: correctAnswer,
+    question,
+    correctAnswer,
   }
 }
--- src/games/progression/generateRound.js
+++ src/games/progression/generateRound.js
@@ -2,7 +2,7 @@ import { countStepProgression, maxStep } from './const.js'
 import { maxRandomNumber } from '../../const.js'
 
 const getProgression = (start, step, length) => {
-  const progression = [0]
+  const progression = []
 
   for (let i = 0; i < length; i++) {
     progression.push(start + i * step)
@@ -15,13 +15,15 @@ export const generateRound = () => {
   const step = Math.floor(Math.random() * maxStep) + 1
   const start = Math.floor(Math.random() * maxRandomNumber)
   const progression = getProgression(start, step, countStepProgression)
+
   const skipElement = Math.floor(Math.random() * progression.length)
-  const correctAnswer = `${progression[skipElement]}`
+  const correctAnswer = String(progression[skipElement])
   progression[skipElement] = '..'
+
   const question = progression.join(' ')
 
   return {
-    question: question,
-    correctAnswer: correctAnswer,
+    question,
+    correctAnswer,
   }
 }
